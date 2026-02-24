ae62cfd8e16fbf34068ecc94558592616d1f63be
Eber_Mk <ebercastano35@gmail.com>
2026-02-24 12:57:56 -0600

    ajustes a la tabla e inportacion del NgFor

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './table.html',
   styleUrls: ['./table.css'],
 })
 export class Table {
-
+    students = [
+      { student_id: "D22390074", name: "John", lastName: "Doe", grade: "10th", group: "A", average: 85 },
+      { student_id: "D22390075", name: "Jane", lastName: "Smith", grade: "11th", group: "B", average: 92 },
+      { student_id: "D22390076", name: "Bob", lastName: "Johnson", grade: "9th", group: "C", average: 78 }
+    ];
 }
